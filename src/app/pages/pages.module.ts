6909ee27488e14bc65d0891ae76d338b32734e2f
Jhampier Giron <jose.giron@packen.co>
2020-04-28 20:34:42 -0500

    Input, Output y viwChild

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from "@angular/core";
 
+import { FormsModule } from '@angular/forms';
+import { ChartsModule } from 'ng2-charts';
+
 import { SharedModule } from "../shared/shared.module";
 
 import { PAGES_ROUTES } from './pages.routes';
@@ -9,21 +12,30 @@ import { ProgressComponent } from "./progress/progress.component";
 import { Graficas1Component } from "./graficas1/graficas1.component";
 import { PagesComponent } from "./pages.component";
 
+//Temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
+
 @NgModule({
     declarations: [
         PagesComponent,
         DashboardComponent,
         ProgressComponent,
         Graficas1Component,
+        IncrementadorComponent,
+        GraficoDonaComponent
     ],
     exports: [
-        DashboardComponent, 
-        ProgressComponent, 
+        DashboardComponent,
+        ProgressComponent,
         Graficas1Component
     ],
     imports: [
-        SharedModule, 
-        PAGES_ROUTES
+        SharedModule,
+        PAGES_ROUTES,
+        FormsModule,
+        ChartsModule
+
     ],
 })
 export class PagesModule { }
